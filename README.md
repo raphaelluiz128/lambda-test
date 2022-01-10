923b4d624af07c8acda9d9cd14b258a05cd2681d
raphael luiz schettino <raphael.schettino@prestador.com.br>
2022-01-10 17:50:07 -0300

    add mysql in readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # lambda-test
+
+#command to add credentials 
+serverless config credentials -o --provider aws --key AKIA6MRIO3HCQ2MQP7GS --secret 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
+
+# database
+
+This project use mysql, use the database with name teste-lambdas and the below code for create table:
+
+create table customers (
+    idCustomer int auto_increment primary key,
+    name varchar (20) not null,
+    email varchar (20) not null
+)
--- handler.js
+++ handler.js
@@ -1,18 +1,14 @@
 'use strict';
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const AWS = require('aws-sdk');
 
+  module.exports.hello = async (event) => {
+    try {}
+    catch(e){
+      console.log(e)
+    }  
+      
+  
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
