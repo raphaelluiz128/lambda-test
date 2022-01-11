8385c8ba31c2408c93002902c2bd36e2de9c2091
raphael luiz schettino <raphael.schettino@prestador.com.br>
2022-01-11 11:49:23 -0300

    add sql query and data from rds

--- handler.js
+++ handler.js
@@ -1,14 +1,29 @@
 'use strict';
 
-const AWS = require('aws-sdk');
-
   module.exports.hello = async (event) => {
-    try {}
+    
+   try {
+          const mysql = require('mysql');
+          const connnection = mysql.createConnection({
+            host: "database-1.cpz8klkkh2jk.sa-east-1.rds.amazonaws.com",
+            user: "admin",
+            password: "12341234",
+            database: "customers"
+          });
+          const p = new Promise((resolve) => {
+            connnection.query("select * from customers", 
+            function(err, results){
+              if(err){ console.log(err); }
+              resolve(results);
+            })
+          });
+          const result = await p;
+            return { 
+              statusCode: 200,
+              body: JSON.stringify({results: result})
+            }     
+        }
     catch(e){
       console.log(e)
     }  
-      
-  
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
